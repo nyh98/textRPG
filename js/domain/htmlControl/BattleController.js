d73f3f75f3fe52d4dc43495f6bf9b003e66b1383
nyh98 <nsi3358@naver.com>
2023-12-13 18:54:32 +0900

    이미지 추가

--- js/domain/htmlControl/BattleController.js
+++ js/domain/htmlControl/BattleController.js
@@ -1,7 +1,16 @@
 class BattleController {
+  #battleService;
   constructor() {
     document.querySelector('#attack').addEventListener(() => this.attack());
+    this.#battleService = new BattleService();
   }
 
-  attack() {}
+  // 사용자랑 상호작용
+  attack() {
+    this.#battleService.attack();
+
+    // 사용자한테 변경된 내용 업데이트
+  }
 }
+
+// 사용자 <-(상호작용)-> Controller <-> Service (실제 로직) <-(데이터 영구 보관)-> Repository
